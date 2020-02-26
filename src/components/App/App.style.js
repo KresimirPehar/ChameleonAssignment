65d3aaf3d518066c77c53bd60f53f28c5bef3c86
QSD_k <info@qsdbih.com>
2020-02-26 15:08:47 +0100

    Delete unnecessary file

--- src/components/App/App.style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import sizeMixin from '../../utils/mixins';
-
-const AppContainer = styled.div`
-  ${sizeMixin('inherit', 'inherit')}
-`;
-
-export default AppContainer;

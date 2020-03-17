daee07abba2d217dad8aa34348d38b3debe8eb4f
QSD_k <info@qsdbih.com>
2020-03-17 14:16:39 +0100

    Remove unnecessary validation

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -35,19 +35,18 @@ const Card = ({
         onDragOver={allowDrop}
         onDrop={onDropHandler}
       >
-        {tasks &&
-          Object.keys(tasks).map(task => (
-            <Task
-              key={tasks[task].id}
-              id={tasks[task].id}
-              value={tasks[task]}
-              onEdit={onEditTask}
-              onDoneUndone={onDoneUndoneTask}
-              onDelete={onDeleteTask}
-              title={title}
-              onAddImage={onAddImage}
-            />
-          ))}
+        {Object.keys(tasks).map(task => (
+          <Task
+            key={tasks[task].id}
+            id={tasks[task].id}
+            value={tasks[task]}
+            onEdit={onEditTask}
+            onDoneUndone={onDoneUndoneTask}
+            onDelete={onDeleteTask}
+            title={title}
+            onAddImage={onAddImage}
+          />
+        ))}
       </Tasks>
     </CardContainer>
   );
